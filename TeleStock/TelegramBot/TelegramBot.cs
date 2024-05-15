c03c669f565a1e0e6bd8f83ba37a386f04cf49a2
ariaskkh <ariaskkh@gmail.com>
2024-05-15 16:39:52 +0900

    폴더명 변경 및 윈폼 .NET framework에서 .NET으로 변경

---
+++ TeleStock/TelegramBot/TelegramBot.cs
@@ -0,0 +1,13 @@
+﻿//using Telegram.Bot;
+
+namespace TelegramBot
+{
+    public class TelegramBot
+    {
+        
+        public void StartTelegramBot()
+        {
+            //var bot = new TelegramBotClient();
+        }
+    }
+}
