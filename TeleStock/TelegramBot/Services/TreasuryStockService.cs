5528c08e1c6d792f0968e405d6eaac5590da9350
dean.kim <ariaskkh@gmail.com>
2024-05-25 17:36:20 +0900

    DetailData 데이터 받아오기

--- TeleStock/TelegramBot/Services/TreasuryStockService.cs
+++ TeleStock/TelegramBot/Services/TreasuryStockService.cs
@@ -1,4 +1,5 @@
 ﻿using Common.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
@@ -15,6 +16,7 @@ namespace TelegramBot.Services
         //private string FILE_PATH = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "../../../../TelegramBot/Data/"));
 
         private List<JsonElement> _overviewData = new();
+        private List<JsonElement> _detailData = new();
 
         public TreasuryStockService(ILogger logger)
         {
@@ -30,19 +32,15 @@ namespace TelegramBot.Services
 
         public async Task GetData()
         {
-            _overviewData = await GetStockOverview();
-            if (_overviewData.Count == 0)
-            {
-                _logger.Log("해당 조건의 overview 데이터가 존재하지 않습니다.");
-                return;
-            }
+            _overviewData = await GetOverviewData();
+            _detailData = await GetDetailData(_overviewData);
         }
 
-        async Task<List<JsonElement>> GetStockOverview()
+        async Task<List<JsonElement>> GetOverviewData()
         {
             var _pageNumber = 1; // 페이지 번호
             var _pageCount = 2; // 페이지 별 건수
-            var startDate = "20240517"; // TEST
+            var startDate = "20240510"; // TEST
             var endDate = GetTodayDate();
             var majorInfoReport = "B001";
             var majorInfoReportUrl = "http://opendart.fss.or.kr/api/list.json";
@@ -55,8 +53,7 @@ namespace TelegramBot.Services
                 ["pblntf_detail_ty"] = majorInfoReport,
             };
 
-            var resultAll = new List<JsonElement>();
-
+            var overviewResult = new List<JsonElement>();
 
             while (true)
             {
@@ -71,14 +68,14 @@ namespace TelegramBot.Services
                         if (HttpStatusCode.OK == response.StatusCode)
                         {
                             string body = await response.Content.ReadAsStringAsync();
-                            using (JsonDocument doc = JsonDocument.Parse(body))
+                            using (var doc = JsonDocument.Parse(body))
                             {
                                 JsonElement overviewData = doc.RootElement.GetProperty("list");
                                 var filteredOverviewData = FilterOverviewData(overviewData);
 
-                                foreach (var item in filteredOverviewData)
+                                foreach (var overviewObj in filteredOverviewData)
                                 {
-                                    resultAll.Add(item.Clone()); // using 밖에서 dispose 되기 때문에 clone() 필요
+                                    overviewResult.Add(overviewObj.Clone()); // using 밖에서 dispose 되기 때문에 clone() 필요
                                 }
 
                                 var totalPage = doc.RootElement.GetProperty("total_page").GetInt32();
@@ -96,23 +93,23 @@ namespace TelegramBot.Services
                 }
                 catch (HttpRequestException ex)
                 {
-                    _logger.Log($"HttpRequestException Message: {ex.Message}");
+                    _logger.Log($"HttpRequestException OverviewData: {ex.Message}");
                     return default;
                 }
                 catch (Exception ex)
                 {
-                    _logger.Log($"Exception: {ex.Message}");
+                    _logger.Log($"Exception - OverviewData: {ex.Message}");
                     return default;
                 }
             }
             
             // TODO: cache check logic
-            return resultAll;
+            return overviewResult;
         }
 
         private List<JsonElement> FilterOverviewData(JsonElement OverviewArray)
         {
-            var keyword = "자기주식";
+            var keyword = "주요사항보고서(자기주식취득결정)"; // TODO: 처분, 신탁도 케이스 대응하기
             var KOSPI = "Y";
             var KOSDAQ = "K";
             List<JsonElement> overviewArray = new();
@@ -122,7 +119,7 @@ namespace TelegramBot.Services
                 string? reportNm = overviewObject.GetProperty("report_nm").GetString();
                 string? corpCls = overviewObject.GetProperty("corp_cls").GetString();
 
-                if (reportNm.Contains(keyword) && (corpCls == KOSPI || corpCls == KOSDAQ))
+                if (reportNm == keyword && (corpCls == KOSPI || corpCls == KOSDAQ))
                 {
                     overviewArray.Add(overviewObject);
                 }
@@ -136,6 +133,69 @@ namespace TelegramBot.Services
         //    string finalJson = JsonSerializer.Serialize(result);
         //    await File.WriteAllTextAsync(Path.Combine(FILE_PATH, "list.json"), finalJson);
         //}
+
+        private async Task<List<JsonElement>> GetDetailData(List<JsonElement> overviewData)
+        {
+            if (_overviewData.Count == 0)
+            {
+                _logger.Log("자기주식 관련 overview 데이터가 존재하지 않습니다.");
+                return default;
+            }
+
+            var companyList = new List<string>();
+            var detailResult = new List<JsonElement>();
+
+            foreach (var overviewObj in overviewData)
+            {
+                var corpCode = overviewObj.GetProperty("corp_code").ToString();
+                var receptData = overviewObj.GetProperty("rcept_dt").ToString();
+
+                if (companyList.Contains(corpCode))
+                    continue;
+                companyList.Add(corpCode);
+
+                var treasuryStockUrl = "https://opendart.fss.or.kr/api/tsstkAqDecsn.json";
+                var parameters = new Dictionary<string, string>
+                {
+                    ["crtfc_key"] = PrivateData.DART_API_KEY,
+                    ["corp_code"] = corpCode,
+                    ["bgn_de"] = receptData,
+                    ["end_de"] = receptData,
+                };
+
+                var queryString = string.Join("&", parameters.Select(KeyValue => $"{Uri.EscapeDataString(KeyValue.Key)}={Uri.EscapeDataString(KeyValue.Value)}"));
+                var url = $"{treasuryStockUrl}?{queryString}";
+
+                try
+                {
+                    using ( var response = await _httpClient.GetAsync(url) )
+                    {
+                        if (HttpStatusCode.OK == response.StatusCode)
+                        {
+                            string body = await response.Content.ReadAsStringAsync();
+                            using (var doc = JsonDocument.Parse(body))
+                            {
+                                // RootElement가 뭔지 확인해보기
+                                JsonElement detailData = doc.RootElement.GetProperty("list");
+                                detailResult.Add(detailData.Clone());
+                            }
+                        }
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    _logger.Log($"HttpRequestException - detailData: {ex.Message}");
+                    return default;
+                }
+                catch (Exception ex)
+                {
+                    _logger.Log($"Exception - detailData: {ex.Message}");
+                    return default;
+                }
+            }
+
+            return detailResult;
+        }
 
         private string GetTodayDate()
         {
