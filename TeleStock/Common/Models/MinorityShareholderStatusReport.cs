b20fa6a28e7f94a57203c239a9e2bcf4f06862ca
dean.kim <ariaskkh@gmail.com>
2024-06-04 20:42:10 +0900

    class 변수 summary 추가

--- TeleStock/Common/Models/MinorityShareholderStatusReport.cs
+++ TeleStock/Common/Models/MinorityShareholderStatusReport.cs
@@ -2,16 +2,27 @@
 {
     public class MinorityShareholderStatusReport
     {
-        public string ReceiptNumber { get; init; } // "rcept_no", 접수번호
-        public CorpClassType CorpClass { get; init; } // "corp_cls", 법인구분
-        public string CorpCode { get; init; } // "corp_code, 고유번호
-        public string CorpName { get; init; } // "corp_name, 법인명
-        public string Separation { get; init; } // "se", 소액주주
-        public string ShareholderCount { get; init; } // "shrholdr_co, 주주수
-        public string ShareholderTotalCount {  get; init; } // "shrholdr_tot_co", 전체 주주수
-        public string ShareholderRate { get; init; } // "shrholdr_rate", 주주 비율
-        public string HoldStockCount { get; init; } // "hold_stock_co", 보유 주식수
-        public string StockTotalCount { get; init; } // "stock_tot_co", 총발행 주식수
-        public string HoldStockRate { get; init; } // "hold_stock_rate", 보유 주식 비율
+        /// <summary> "rcept_no", 접수번호 </summary>
+        public string ReceiptNumber { get; init; }
+        /// <summary> "corp_cls", 법인구분 </summary>
+        public CorpClassType CorpClass { get; init; }
+        /// <summary> "corp_code, 고유번호 </summary>
+        public string CorpCode { get; init; }
+        /// <summary> "corp_name, 법인명 </summary>
+        public string CorpName { get; init; }
+        /// <summary> "se", 소액주주  </summary>
+        public string Separation { get; init; } 
+        /// <summary> "shrholdr_co, 주주수 </summary>
+        public string ShareholderCount { get; init; }
+        /// <summary> "shrholdr_tot_co", 전체 주주수 </summary>
+        public string ShareholderTotalCount { get; init; }
+        /// <summary> "shrholdr_rate", 주주 비율 </summary>
+        public string ShareholderRate { get; init; }
+        /// <summary> "hold_stock_co", 보유 주식수 </summary>
+        public string HoldStockCount { get; init; }
+        /// <summary> "stock_tot_co", 총발행 주식수 </summary>
+        public string StockTotalCount { get; init; }
+        /// <summary> "hold_stock_rate", 보유 주식 비율 </summary>
+        public string HoldStockRate { get; init; } 
     }
 }
--- TeleStock/Common/Models/TreasuryDetailReport.cs
+++ TeleStock/Common/Models/TreasuryDetailReport.cs
@@ -2,37 +2,61 @@
 {
     public class TreasuryDetailReport 
     {
-        // 사용 O
-        public string ReceiptNumber { get; init; } // rcept_no, 접수번호
-        public string AuditAttendance { get; init; } // adt_a_atn, 감사(사외이사가 아닌 감사위원)참석여부
-        public string AcquisitionDate { get; init; } // aq_dd
-        public string AcquisitionMethod { get; init; } // aq_mth
-        public string AcquisitionPurpose { get; init; } // aq_pp
-        public string AcquisitionWithinDevidendOrdinaryStock { get; init; } // aq_wtn_div_ostk, 취득 전 자기주식 보유현황 (배당가능 범위 내 취득)(보통주식)
-        public string AcquisitionWithinDevidendOrdinaryStockRate { get; init; } // aq_wtn_div_ostk_rt, 취득 전 자기주식 보유현황(배당가능이익 범위 내 취득(주)(비율(%)))
-        public string AcquisitionWithinDevidendExtraordinaryStock { get; init; } // aq_wtn_div_estk, 취득 전 자기주식 보유현황(배당가능이익 범위 내 취득(주)(기타주식))
-        public string AcquisitionWithinDevidendExtraordinaryStockRate { get; init; } // aq_wtn_div_estk_rt, 취득 전 자기주식 보유현황(배당가능이익 범위 내 취득(주)(비율(%)))
-        public string ExpectedAcquisitionStartDate { get; init; } // aqexpd_bgd
-        public string ExpectedAcquisitionEndDate { get; init; } // aqexpd_edd
-        public string AcquisitionPriceOfOrdinaryStock { get; init; } // aqpln_prc_ostk
-        public string AcquisitionPriceOfExtraordinaryStock { get; init; } // aqpln_prc_estk
-        public string AcquisitionNumberOfOrdinaryStock { get; init; } // aqpln_stk_ostk
-        public string AcquisitionNumberOfExtraordinaryStock { get; init; } // aqpln_stk_estk
+        /// <summary> rcept_no, 접수번호 </summary>
+        public string ReceiptNumber { get; init; }
+        /// <summary> adt_a_atn, 감사(사외이사가 아닌 감사위원)참석여부 </summary>
+        public string AuditAttendance { get; init; }
+        /// <summary> aq_dd </summary>
+        public string AcquisitionDate { get; init; }
+        /// <summary> aq_mth </summary>
+        public string AcquisitionMethod { get; init; }
+        /// <summary> aq_pp  </summary>
+        public string AcquisitionPurpose { get; init; }
+        /// <summary> aq_wtn_div_ostk, 취득 전 자기주식 보유현황 (배당가능 범위 내 취득)(보통주식) </summary>
+        public string AcquisitionWithinDevidendOrdinaryStock { get; init; }
+        /// <summary> aq_wtn_div_ostk_rt, 취득 전 자기주식 보유현황(배당가능이익 범위 내 취득(주)(비율(%))) </summary>
+        public string AcquisitionWithinDevidendOrdinaryStockRate { get; init; }
+        /// <summary> aq_wtn_div_estk, 취득 전 자기주식 보유현황(배당가능이익 범위 내 취득(주)(기타주식)) </summary>
+        public string AcquisitionWithinDevidendExtraordinaryStock { get; init; }
+        /// <summary> aq_wtn_div_estk_rt, 취득 전 자기주식 보유현황(배당가능이익 범위 내 취득(주)(비율(%))) </summary>
+        public string AcquisitionWithinDevidendExtraordinaryStockRate { get; init; }
+        /// <summary> aqexpd_bgd </summary>
+        public string ExpectedAcquisitionStartDate { get; init; }
+        /// <summary> aqexpd_edd </summary>
+        public string ExpectedAcquisitionEndDate { get; init; }
+        /// <summary> aqpln_prc_ostk </summary>
+        public string AcquisitionPriceOfOrdinaryStock { get; init; }
+        /// <summary> aqpln_prc_estk </summary>
+        public string AcquisitionPriceOfExtraordinaryStock { get; init; }
+        /// <summary> aqpln_stk_ostk </summary>
+        public string AcquisitionNumberOfOrdinaryStock { get; init; }
+        /// <summary> aqpln_stk_estk </summary>
+        public string AcquisitionNumberOfExtraordinaryStock { get; init; } 
         public CorpClassType CorpClass { get; init; }
         public string CorpCode { get; init; }
         public string CorpName { get; init; }
-        public string ConsignmentInvenstmentBrokerage { get; init; } // cs_iv_bk, 위탁 중개 사업자
-        public string PurchaseLimitPerDayOfOrdinaryStock { get; init; } // d1_prodlm_ostk, 1일 매수 주문수량 한도(보통주식)
-        public string PurchaseLimitPerDayOfExtraordinaryStock { get; init; } // d1_prodlm_estk, 1일 매수 주문수량 한도(기타주식)
-        public string ExtraAcquisitionOrdinaryStock { get; init; } // eaq_ostk, 취득 전 자기주식 보유현황(기타취득(주)(보통주식))
-        public string ExtraAcquisitionOrdinaryStockRate { get; init; } // eaq_ostk_rt, 취득 전 자기주식 보유현황(기타취득(주)(비율 %))
-        public string ExtraAcquisitionExtraordinaryStock { get; init; } // eaq_estk, 취득 전 자기주식 보유현황(기타취득(주)(기타주식))
-        public string ExtraAcquisitionExtraordinaryStockRate { get; init; } // eaq_estk_rt, 취득 전 자기주식 보유현황(기타취득(주)(비율 %))
-        public string ExpectedHoldingPeriodStartData { get; init; } // hdexpd_bgd, 보유예상기간(시작일)
-        public string ExpectedHoldingPeriodEndData { get; init; } // hdexpd_edd, 보유예상기간(종료일)
-        public string OutsideDirectorAttendenceCount { get; init; } // od_a_at_t, 사외이사참석여부(참석(명))
-        public string OutsideDirectorAbsentCount { get; init; } // od_a_at_b, 사외이사참석여부(참석(명))
-
+        /// <summary> cs_iv_bk, 위탁 중개 사업자 </summary>
+        public string ConsignmentInvenstmentBrokerage { get; init; }
+        /// <summary> d1_prodlm_ostk, 1일 매수 주문수량 한도(보통주식) </summary>
+        public string PurchaseLimitPerDayOfOrdinaryStock { get; init; }
+        /// <summary> d1_prodlm_estk, 1일 매수 주문수량 한도(기타주식) </summary>
+        public string PurchaseLimitPerDayOfExtraordinaryStock { get; init; }
+        /// <summary> eaq_ostk, 취득 전 자기주식 보유현황(기타취득(주)(보통주식)) </summary>
+        public string ExtraAcquisitionOrdinaryStock { get; init; }
+        /// <summary> eaq_ostk_rt, 취득 전 자기주식 보유현황(기타취득(주)(비율 %)) </summary>
+        public string ExtraAcquisitionOrdinaryStockRate { get; init; }
+        /// <summary> eaq_estk, 취득 전 자기주식 보유현황(기타취득(주)(기타주식)) </summary>
+        public string ExtraAcquisitionExtraordinaryStock { get; init; }
+        /// <summary> eaq_estk_rt, 취득 전 자기주식 보유현황(기타취득(주)(비율 %)) </summary>
+        public string ExtraAcquisitionExtraordinaryStockRate { get; init; }
+        /// <summary> hdexpd_bgd, 보유예상기간(시작일) </summary>
+        public string ExpectedHoldingPeriodStartData { get; init; }
+        /// <summary> hdexpd_edd, 보유예상기간(종료일) </summary>
+        public string ExpectedHoldingPeriodEndData { get; init; }
+        /// <summary> od_a_at_t, 사외이사참석여부(참석(명)) </summary>
+        public string OutsideDirectorAttendenceCount { get; init; }
+        /// <summary> od_a_at_b, 사외이사참석여부(참석(명)) </summary>
+        public string OutsideDirectorAbsentCount { get; init; }
         
         public bool IsOrdinaryStock => AcquisitionNumberOfOrdinaryStock != "-" || AcquisitionPriceOfOrdinaryStock != "-";
 
@@ -41,7 +65,6 @@
             if (IsOrdinaryStock)
             {
                 return Math.Round((double)(long.Parse(AcquisitionPriceOfOrdinaryStock.Replace(",", "")) / 100000000)).ToString();
-                
             }
             else
             {
