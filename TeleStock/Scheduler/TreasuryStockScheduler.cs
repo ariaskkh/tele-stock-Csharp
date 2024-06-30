b3091b398e2888d5ed40ac3b49f5e78d35799e19
dean.kim <ariaskkh@gmail.com>
2024-06-30 23:37:40 +0900

    IsWorkingHour() 에만 동작하도록 로직 추가

--- TeleStock/Scheduler/TreasuryStockScheduler.cs
+++ TeleStock/Scheduler/TreasuryStockScheduler.cs
@@ -6,9 +6,10 @@ namespace Scheduler
     public class TreasuryStockScheduler
     {
         private ILogger _logger;
-        private static System.Timers.Timer timer;
+        private static System.Timers.Timer? timer;
         private Func<Task> _action;
-        private bool isRunning = false;
+        private static readonly SemaphoreSlim semaphore = new SemaphoreSlim(1);
+        private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
 
         public TreasuryStockScheduler(ILogger logger)
         {
@@ -23,9 +24,13 @@ namespace Scheduler
 
         public void Stop()
         {
-            _logger.Log("멈춤");
-            timer.Stop();
-            timer.Dispose();
+            if (timer != null)
+            {
+                _logger.Log("멈춤");
+                timer.Stop();
+                timer.Dispose();
+                cancellationTokenSource.Cancel();
+            }
         }
 
         private void StartScheduler()
@@ -37,24 +42,80 @@ namespace Scheduler
             timer.Enabled = true;
         }
 
+        // 얘 메인 스레드인지 백그라운드 스레드인지 확인해야함. 아마 백그라운드 스레드 같다.
         private async Task OnTimedEvent(Object? source, ElapsedEventArgs e)
         {
-            if (isRunning)
+            if (IsWorkingHour())
             {
-                return;
-            };
-
-            try
-            {
-                _logger.Log("데이터 받아오기");
-                if (_action != null)
+                await semaphore.WaitAsync();
                 {
-                    await _action();
+                    try
+                    {
+                        if (_action != null)
+                        {
+                            _logger.Log("데이터 받아오기");
+                            await _action();
+                        }
+                    }
+                    catch
+                    {
+                        _logger.Log("데이터 받아오기 실패");
+                    }
+                    finally
+                    {
+                        semaphore.Release();
+                    }
+                }
+                
+            }
+            else
+            {
+                if (timer != null)
+                {
+                    timer.Stop();
+                    await SleepUntilWorkingHour();
+                    timer.Start();
                 }
             }
-            finally
+        }
+
+        private bool IsWorkingHour()
+        {
+            TimeZoneInfo koreanTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Korea Standard Time");
+
+            DateTime koreanTimeNow = TimeZoneInfo.ConvertTime(DateTime.Now, koreanTimeZone);
+
+            TimeSpan workStart = new TimeSpan(9, 0, 0); // 시간
+            TimeSpan workEnd = new TimeSpan(15, 30, 0);
+
+            return koreanTimeNow.TimeOfDay >= workStart && koreanTimeNow.TimeOfDay <= workEnd;
+        }
+
+        private async Task SleepUntilWorkingHour()
+        {
+            TimeZoneInfo koreanTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Korea Standard Time");
+
+            DateTime koreanTimeNow = TimeZoneInfo.ConvertTime(DateTime.Now, koreanTimeZone);
+            DateTime nextStart = koreanTimeNow.Date.AddHours(9); // 시간
+
+            if (koreanTimeNow.TimeOfDay >= new TimeSpan(15, 30, 0))
             {
-                isRunning = false;
+                nextStart = nextStart.AddDays(1);
+            }
+
+            TimeSpan sleepTime = nextStart - DateTime.Now;
+
+            if (sleepTime >= TimeSpan.Zero)
+            {
+                _logger.Log($"Sleeping for {sleepTime.TotalMinutes} minutes ");
+                try
+                {
+                    await Task.Delay(sleepTime, cancellationTokenSource.Token);
+                }
+                catch (TaskCanceledException)
+                {
+                    _logger.Log("Sleep was canceled");
+                }
             }
         }
     }
