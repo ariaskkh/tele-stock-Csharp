e70467944ae03225819197922ec333cc4484b737
ariaskkh <ariaskkh@gmail.com>
2024-07-13 22:52:11 +0900

    임시 DDB

---
+++ TeleStock/Common/Repository/TreasuryStockRepositoryDDB.cs
@@ -0,0 +1,38 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Runtime;
+
+namespace Common.Repository;
+
+public class TreasuryStockRepositoryDDB
+{
+    private static AmazonDynamoDBClient client;
+
+    public TreasuryStockRepositoryDDB()
+    {
+        var config = new AmazonDynamoDBConfig
+        {
+            ServiceURL = "http://localhost:8000"
+        };
+
+        var credentials = new BasicAWSCredentials("fakeMyKeyId", "fakeSecretAccessKey");
+
+        client = new AmazonDynamoDBClient(credentials, config);
+    }
+
+    public void CreateTable()
+    {
+
+    }
+    
+}
+
+[DynamoDBTable("Example")]
+public class Example
+{
+    [DynamoDBHashKey]
+    public string Id { get; set; }
+
+    [DynamoDBProperty]
+    public string data { get; set; }
+}
